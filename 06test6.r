7600d71442fc3cddd9246cf605b5d058dd64f18a
SRIHAAS-PIGILAM <srihaas.pigilam@gmail.com>
2023-10-19 18:34:15 +0530

    R Programming

---
+++ 06test6.r
@@ -0,0 +1,24 @@
+var1 <- var2 <- var4 <- var3 <- "Hi"
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+
+#Note: <<- is a global assigner. You will learn more about this in the Global Variable chapter.
+
+#It is also possible to turn the direction of the assignment operator.
+
+#x <- 3 is equal to 3 -> x
+
+my_var <- 3
+
+my_var <<- 3
+
+3 -> my_var
+
+3 ->> my_var
+
+print(my_var) # print my_var
+
